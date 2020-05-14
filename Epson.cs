7ca6832a1a1783f01d81af4e0f4be4d8ae05e37f
hernanda2829 <hernanda.pramudita@students.amikom.ac.id>
2020-05-14 14:06:19 +0700

    inisialisasi project

---
+++ Epson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polymorphisminterface;
+
+namespace Polymorphisminterface
+{
+    public class Epson : IPrinterWindows
+    {
+        public void print()
+        {
+            Console.WriteLine("Epson display dimension : 10*11");
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Epson printer printing . . . .");
+        }
+    }
+}
---
+++ LaserJet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polymorphisminterface;
+
+namespace Polymorphisminterface
+{
+    public class LaserJet : IPrinterWindows
+    {
+        public void print()
+        {
+            Console.WriteLine("LaserJet display dimension : 12*12");
+        }
+
+        public void show()
+        {
+            Console.WriteLine("LaserJet printer printing . . . .");
+        }
+    }
+}
